6b65bf9acf522c293a3c2167c1c29687ed17ffd5
Gabrielle von Koss <gabivk@gmail.com>
2018-10-09 17:09:51 +0100

    implement scroll on primary

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 import * as oHeader from 'o-header'; // eslint-disable-line no-unused-vars
+import scroll from './src/js/scroll'
+
+const headerEl = window.h = document.querySelector('.o-header-services');
+
+scroll.init(headerEl);
---
+++ src/js/scroll.js
@@ -0,0 +1,76 @@
+import * as oUtils from 'o-utils';
+
+function init(headerEl) {
+	const container = headerEl.querySelector('[data-o-header-services-nav]');
+
+	if (container === null) {
+		return;
+	}
+
+	const buttons = Array.from(container.getElementsByTagName('button'));
+	const list = window.l = container.querySelector('.o-header-services__nav-list');
+
+	let scrollWidth;
+	let listWidth = list.clientWidth;
+
+	function checkCurrentPosition() {
+		const currentSelection = list.querySelector('[aria-current]');
+		if (currentSelection) {
+			let currentSelectionEnd = currentSelection.getBoundingClientRect().right;
+
+			//if the current selection is wider than the end of the list
+			if (currentSelectionEnd > listWidth) {
+				// check by how much
+				let diff = currentSelectionEnd - listWidth;
+				// if the difference is greater than half of the list, scroll to the end of the current selection.
+				diff = (diff > listWidth / 2) ? currentSelectionEnd : listWidth / 2;
+
+				list.scrollTo(diff, 0);
+			}
+		}
+		scrollable();
+	}
+
+	function direction(button) {
+		return button.className.match(/left|right/).pop();
+	}
+
+	function scrollable() {
+		scrollWidth = list.scrollWidth;
+
+		buttons.forEach(button => {
+			if (direction(button) === 'left') {
+				button.disabled = list.scrollLeft === 0;
+			} else {
+				const remaining = scrollWidth - listWidth - list.scrollLeft;
+				// Allow a little difference as scrollWidth is fast, not accurate.
+				button.disabled = remaining <= 1;
+			}
+		});
+	}
+
+	function scroll(e) {
+		let distance = 100;
+
+		if (direction(e.currentTarget) === 'left') {
+			distance = (list.scrollLeft > distance ? distance : list.scrollLeft) * -1;
+		} else {
+			const remaining = scrollWidth - listWidth - list.scrollLeft;
+			distance = remaining > distance ? distance : remaining;
+		}
+
+		list.scrollLeft = list.scrollLeft + distance;
+
+		scrollable();
+	}
+
+	list.addEventListener('scroll', oUtils.throttle(scrollable, 100));
+
+	buttons.forEach(button => {
+		button.onclick = scroll;
+	});
+
+	checkCurrentPosition();
+}
+
+export default { init };
