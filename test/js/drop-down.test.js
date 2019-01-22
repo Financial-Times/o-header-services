2fd43fedc130448676ebd1259eb375a0a9371d46
Gabrielle von Koss <gabivk@gmail.com>
2019-01-22 10:59:11 +0000

    add test

--- test/js/drop-down.test.js
+++ test/js/drop-down.test.js
@@ -25,31 +25,40 @@ describe('Dropdown', () => {
 		beforeEach(() => {
 			new HeaderServices(headerEl);
 			navItems = document.querySelectorAll('li[data-o-header-services-level="1"]');
-			click = (i, element) => navItems[i].querySelector(element).dispatchEvent(new Event('click'));
+			click = (parent, element) => parent.querySelector(element).dispatchEvent(new Event('click'));
 		});
 
-		it('display nav dropdown on click', () => {
-			click(0, 'a');
+		it('open on click', () => {
+			click(navItems[0], 'a');
 			attribute = navItems[0].getAttribute('aria-expanded') === 'true';
 			proclaim.isTrue(attribute);
 		});
 
-		it('hides nav dropdown on double click', () => {
-			click(0, 'a');
-			click(0, 'a');
+		it('hides on double click', () => {
+			click(navItems[0], 'a');
+			click(navItems[0], 'a');
 			attribute = navItems[0].getAttribute('aria-expanded') === 'false';
 			proclaim.isTrue(attribute);
 		});
 
 		it('hides open dropdowns when different nav item is toggled', () => {
-			click(0, 'a');
+			click(navItems[0], 'a');
 			attribute = navItems[0].getAttribute('aria-expanded') === 'true';
 			proclaim.isTrue(attribute);
-			click(1, 'a');
+			click(navItems[1], 'a');
 			attribute = navItems[0].getAttribute('aria-expanded') === 'false';
 			proclaim.isTrue(attribute);
 			attribute = navItems[1].getAttribute('aria-expanded') === 'true';
 			proclaim.isTrue(attribute);
 		});
+
+		it('hides all menus if click outside of nav items', () => {
+			click(navItems[0], 'a');
+			attribute = navItems[0].getAttribute('aria-expanded') === 'true';
+			proclaim.isTrue(attribute);
+			click(document, 'body');
+			attribute = navItems[0].getAttribute('aria-expanded') === 'false';
+			proclaim.isTrue(attribute);
+		})
 	});
 });
